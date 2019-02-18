217a640f9e5433d02a542aed61313e62678fd287
Paul Frazee <pfrazee@gmail.com>
2019-02-18 10:03:37 -0600

    Add app-menu component

---
+++ js/com/app-menu.js
@@ -0,0 +1,69 @@
+import {html} from '../../vendor/lit-element/lit-element.js'
+import * as contextMenu from './context-menu.js'
+
+export function create ({x, y}) {
+  return contextMenu.create({
+    x,
+    y,
+    render () {
+      return html`
+        <style>
+          .appmenu {
+            display: grid;
+            grid-template-columns: 1fr 1fr 1fr;
+            padding: 20px 20px;
+          }
+          .appmenu .dropdown-item {
+            font-size: 14px;
+            text-align: center;
+
+            /* all same dimensions */
+            padding: 7px 15px;
+            width: 80px;
+            line-height: 40px;
+            
+            /* remove link styles */
+            color: #444;
+            text-decoration: none;
+
+            /* remove dropdown-item border */
+            border: 0;
+          }
+          .appmenu .dropdown-item img {
+            width: 32px;
+            height: 32px;
+
+            /* center */
+            display: block;
+            margin: 0 auto;
+          }
+          .appmenu .dropdown-item img.profile {
+            border-radius: 50%;
+          }
+        </style>
+        <div class="appmenu dropdown-items right">
+          <a class="dropdown-item" href="dat://feed">
+            <img src="/vendor/beaker-app-stdlib/img/icons/newsfeed.png">
+            News Feed
+          </a>
+          <a class="dropdown-item" href="dat://library">
+            <img src="/vendor/beaker-app-stdlib/img/icons/library.png">
+            Library
+          </a>
+          <a class="dropdown-item" href="dat://search">
+            <img src="/vendor/beaker-app-stdlib/img/icons/search.png">
+            Search
+          </a>
+          <a class="dropdown-item" href="dat://bookmarks">
+            <img src="/vendor/beaker-app-stdlib/img/icons/bookmarks.png">
+            Bookmarks
+          </a>
+          <a class="dropdown-item" href="dat://profile">
+            <img class="profile" src="/img/tmp-profile.png">
+            Your Profile
+          </a>
+        </div>
+      `
+    }
+  })
+}
