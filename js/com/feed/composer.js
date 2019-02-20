7206b0b4d69d3a47013cd4afbfc7aa33cd2d5ebf
Paul Frazee <pfrazee@gmail.com>
2019-02-20 09:34:18 -0600

    Add the submit event to beaker-feed-composer component

--- js/com/feed/composer.js
+++ js/com/feed/composer.js
@@ -18,6 +18,12 @@ export class FeedComposer extends LitElement {
     this.draftText = ''
   }
 
+  _submit () {
+    if (!this.draftText) return
+    this.dispatchEvent(new CustomEvent('submit', {detail: {body: this.draftText}}))
+    this.draftText = ''
+  }
+
   // rendering
   // =
 
@@ -44,13 +50,17 @@ export class FeedComposer extends LitElement {
     return html`
       <textarea
         placeholder="What's on your mind?"
-        @keydown=${this.onChangeTextarea}
+        @keydown=${this.onKeydownTextarea}
         @keyup=${this.onChangeTextarea}
         @blur=${this.onBlurTextarea}
       >${this.draftText}</textarea>
       <div class="actions">
         <span class="${charCountCls}">${this.draftText.length} / ${CHAR_LIMIT}</span>
-        <button class="btn primary" ?disabled=${this.draftText.length === 0}>Post</button>
+        <button
+          class="btn primary"
+          ?disabled=${this.draftText.length === 0}
+          @click=${this.onClickPost}
+        >Post</button>
       </div>
     `
   }
@@ -66,6 +76,17 @@ export class FeedComposer extends LitElement {
     this.shadowRoot.querySelector('textarea').focus()
   }
 
+  onKeydownTextarea (e) {
+    // check for cmd/ctrl+enter
+    if (e.key === 'Enter' && (e.ctrlKey || e.metaKey)) {
+      e.preventDefault()
+      e.currentTarget.value = ''
+      e.currentTarget.blur()
+      return this._submit()
+    }
+    this.onChangeTextarea(e)
+  }
+
   onChangeTextarea (e) {
     if (e.currentTarget.value.length > CHAR_LIMIT) {
       this.draftText = e.currentTarget.value = e.currentTarget.value.slice(0, CHAR_LIMIT)
@@ -77,6 +98,10 @@ export class FeedComposer extends LitElement {
 
   onBlurTextarea () {
     this.isFocused = false
+  }
+
+  onClickPost () {
+    this._submit()
   }
 }
 FeedComposer.styles = feedComposerCSS
