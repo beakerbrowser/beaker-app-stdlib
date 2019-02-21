6d5c5de2c84c6a956f036d55eb7b71ca87a7e3c2
Paul Frazee <pfrazee@gmail.com>
2019-02-21 14:42:00 -0600

    Add toNiceUrl, makeSafe, and highlightSearchResult methods to strings.js

--- js/strings.js
+++ js/strings.js
@@ -32,3 +32,30 @@ export function toNiceDomain (str) {
   }
   return str
 }
+
+export function toNiceUrl (str) {
+  if (!str) return ''
+  try {
+    var urlParsed = new URL(str)
+    if (DAT_KEY_REGEX.test(urlParsed.hostname)) {
+      urlParsed.hostname = `${urlParsed.hostname.slice(0, 4)}..${urlParsed.hostname.slice(-2)}`
+    }
+    return urlParsed.toString()
+  } catch (e) {
+    // ignore, not a url
+  }
+  return str
+}
+
+export function makeSafe (str = '') {
+  return str.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/&/g, '&amp;').replace(/"/g, '&quot;')
+}
+
+// search results are returned from beaker's search APIs with nonces wrapping the highlighted sections
+// e.g. a search for "test" might return "the {500}test{/500} result"
+// this enables us to safely escape the HTML, then replace the nonces with <strong> tags
+export function highlightSearchResult (str = '', nonce = 0) {
+  var start = new RegExp(`\\{${nonce}\\}`, 'g') // eg {500}
+  var end = new RegExp(`\\{/${nonce}\\}`, 'g') // eg {/500}
+  return makeSafe(str).replace(start, '<strong>').replace(end, '</strong>')
+}
