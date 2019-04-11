162021ccc780a943056822c402aadc739ab2718a
Paul Frazee <pfrazee@gmail.com>
2019-04-11 10:39:53 -0500

    Update the app-menu com

--- js/com/app-menu.js
+++ js/com/app-menu.js
@@ -5,56 +5,36 @@ export function create ({x, y}) {
   return contextMenu.create({
     x,
     y,
+    noBorders: true,
     render () {
       return html`
         <style>
-          .appmenu {
-            display: grid;
-            grid-template-columns: 1fr 1fr 1fr;
-            padding: 20px 20px;
-          }
-          .appmenu .dropdown-item {
+          .app-menu {
+            padding: 4px 0;
+            min-width: 200px;
             font-size: 14px;
-            text-align: center;
-
-            /* all same dimensions */
-            padding: 7px 15px;
-            width: 80px;
-            line-height: 40px;
-            
-            /* remove link styles */
-            color: #444;
-            text-decoration: none;
-
-            /* remove dropdown-item border */
-            border: 0;
           }
-          .appmenu .dropdown-item img {
+          .app-menu .dropdown-item {
+            display: flex;
+            align-items: center;
+
+            /* remove link styles */
+            color: #000;
+            text-decoration: none;
+          }
+          .app-menu .dropdown-item img {
             width: 32px;
             height: 32px;
-
-            /* center */
-            display: block;
-            margin: 0 auto;
-          }
-          .appmenu .dropdown-item img.profile {
-            border-radius: 50%;
+            top: 0;
           }
         </style>
-        <div class="appmenu dropdown-items right">
-          <a class="dropdown-item" href="beaker://bookmarks">
-            <img src="/vendor/beaker-app-stdlib/img/icons/bookmarks.png">
-            Bookmarks
-          </a>
-          <a class="dropdown-item" href="beaker://library">
+        <div class="app-menu dropdown-items with-triangle no-border right">
+          <div class="section-header small light">Applications</div>
+          <a class="dropdown-item" href="beaker://library/">
             <img src="/vendor/beaker-app-stdlib/img/icons/library.png">
             Library
           </a>
-          <a class="dropdown-item" href="beaker://search">
-            <img src="/vendor/beaker-app-stdlib/img/icons/search.png">
-            Search
-          </a>
-          <a class="dropdown-item" href="intent:unwalled.garden/view-feed">
+          <a class="dropdown-item" href="dat://beaker.social/">
             <img src="/vendor/beaker-app-stdlib/img/icons/newsfeed.png">
             Beaker.Social
           </a>
